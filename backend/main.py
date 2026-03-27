6d4418e51bbf28836e71fe6033f6dece196962af
damjali <adamrazali54@gmail.com>
2026-03-27 19:58:54 +0800

    structured everything fixed

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Hello World 🚀"}
+
+# SemakMule Endpoint
+@app.get("/api/check/phone_number/{number}")
+def get_item(phoneNum: int):
+    return {
+        "phone_number": phoneNum
+    }
+
+@app.get("/api/check/bank_number/{number}")
+def get_item(bankNum: int):
+    return {
+        "bank_number": bankNum
+    }
+
+@app.get("/api/check/email/{email}")
+def get_item(email: str):
+    return {
+        "email": email
+    }
+
+# Phishing Detection Endpoint
+@app.post("/api/check/phishing")
+def create_item(sentence: str):
+    return {
+        "result": True
+    }
